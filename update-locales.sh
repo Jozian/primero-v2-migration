9e3a46607824ce0f609fcd8d718a2d762c1c2835
Rodrigo Belem <rodrigo.belem@gmail.com>
2021-07-15 01:07:12 -0400

    Add a script to update the locales strings to the ruby format

---
+++ update-locales.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+BASEDIR=$1
+LC_SRC=$2
+LC_DST=$3
+
+find $BASEDIR -type f -name "*.rb" -exec sed -r "s/([_0-9a-zA-Z]*)_"$LC_SRC"/\'\1_"$LC_DST"\'/g" -i {} \;
+find $BASEDIR -type f -name "*.rb" -exec sed -r 's/'"$LC_SRC"'/'"$LC_DST"'/g' -i {} \;
