84bc93bd1454202e63e81492982ac5ae374aefe5
Rodrigo Belem <>
2021-10-13 16:13:09 -0400

    Fix error when locale is only two letters

--- update-locales.sh
+++ update-locales.sh
@@ -4,5 +4,5 @@ BASEDIR=$1
 LC_SRC=$2
 LC_DST=$3
 
-find $BASEDIR -type f -name "*.rb" -exec sed -r "s/([_0-9a-zA-Z]*)_"$LC_SRC"/\'\1_"$LC_DST"\'/g" -i {} \;
-find $BASEDIR -type f -name "*.rb" -exec sed -r 's/'"$LC_SRC"'/'"$LC_DST"'/g' -i {} \;
+find $BASEDIR -type f -name "*.rb" -not -name "load_configuration.rb" -exec sed -r "s/([_0-9a-zA-Z]*)_"$LC_SRC":/'\1_"$LC_DST"':/g" -i {} \;
+find $BASEDIR -type f -name "*.rb" -exec sed -r "s/\""$LC_SRC"\"/'"$LC_DST"'/g" -i {} \;
